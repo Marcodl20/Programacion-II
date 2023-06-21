34551da455fc854736c4e8c93e1331d3c922fe17
Marcodl20 <marco.lara01@epn.edu.ec>
2023-06-20 19:34:20 -0500

    Updated

---
+++ src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hola, carajo si logre subir el archivo");
+        System.out.println("Debo practicar mas xdddd");
+    }
+}
